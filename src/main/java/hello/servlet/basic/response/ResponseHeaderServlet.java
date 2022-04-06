5a6e7009e1bf6cfbf02c06a3334d45b9055136ad
username <user@email.mail>
2022-04-07 02:04:13 +0900

    commit

--- src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
+++ src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -2,6 +2,7 @@ package hello.servlet.basic.response;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -25,7 +26,10 @@ public class ResponseHeaderServlet extends HttpServlet {
 
         //[Header 편의 메서드]
         content(response);
+        cookie(response);
+        redirect(response);
 
+        //[message body]
         PrintWriter writer = response.getWriter();
         writer.println("ok");
     }
@@ -37,5 +41,21 @@ public class ResponseHeaderServlet extends HttpServlet {
         response.setContentType("text/plain");
         response.setCharacterEncoding("utf-8");
         //response.setContentLength(2); //(생략시 자동 생성)
+    }
+
+    private void cookie(HttpServletResponse response) {
+        //Set-Cookie: myCookie=good; Max-Age=600;
+        //response.setHeader("Set-Cookie", "myCookie=good; Max-Age=600");
+        Cookie cookie = new Cookie("myCookie", "good");
+        cookie.setMaxAge(600); //600초
+        response.addCookie(cookie);
+    }
+
+    private void redirect(HttpServletResponse response) throws IOException {
+        //Status Code 302
+        //Location: /basic/hello-form.html
+        //response.setStatus(HttpServletResponse.SC_FOUND); //302
+        //response.setHeader("Location", "/basic/hello-form.html");
+        response.sendRedirect("/basic/hello-form.html");
     }
 }
---
+++ src/main/java/hello/servlet/domain/member/MemberRepository.java
@@ -0,0 +1,48 @@
+package hello.servlet.domain.member;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 동시성 문제가 고려되어 있지 않음, 실무에서는 ConcurrentHashMap, AtomicLong 사용 고려
+ */
+
+public class MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    //싱글톤
+    private static final MemberRepository instance = new MemberRepository();
+
+    //무조건 이 메서드로 조회해야 함
+    public static MemberRepository getInstance(){
+        return instance;
+    }
+
+    private MemberRepository(){
+
+    }
+
+    public Member save(Member member){
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findById(Long id){
+        return store.get(id);
+    }
+
+    public List<Member> findAll(){
+        //store의 있는 모든 값들을 꺼내서 새로운 ArrayList<>에 넣어준다.
+        return new ArrayList<>(store.values());
+    }
+
+    //테스트할 때 사용, store를 다 날려버림
+    public void clearStore(){
+        store.clear();
+    }
+}
